8671acefb8b3779ed622548d82663db1a65809d5
Touko Toivonen <touko.t.toivonen@gmail.com>
2026-09-08 10:45:15 +0300

    teht 2.7 done

---
+++ part2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+    };
+    console.log("ennen henkilön lisäämistä", nameObject);
+    const nameOnList = persons.some((person) => person.name === newName);
+
+    if (nameOnList) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+    }
+  };
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((persons) => (
+          <li key={persons.name}>{persons.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
